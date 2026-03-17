0b07a230045228bdc53ed51d294dc6f734a40afa
Callum MacLeod <cmcallmacleod@gmail.com>
2026-03-17 09:27:10 -0400

    Add tailwind linting

--- config/ci.rb
+++ config/ci.rb
@@ -6,6 +6,7 @@ CI.run do
   step "Style: Ruby", "bin/rubocop"
   step "Style: ERB", "bundle exec herb analyze app"
   step "Style: ERB Lint", "npm run herb:lint"
+  step "Style: Tailwind", "npm run tailwind:lint"
 
   step "Security: Gem audit", "bin/bundler-audit"
   step "Security: Importmap vulnerability audit", "bin/importmap audit"
