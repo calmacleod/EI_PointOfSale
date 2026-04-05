0360d3877aed463b257af45215f0ebea4801293d
Callum MacLeod <cmcallmacleod@gmail.com>
2026-04-05 15:27:48 -0400

    Update bundle

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -3,7 +3,7 @@
 # Pagy initializer (43.2.9)
 # See https://ddnexus.github.io/pagy/resources/initializer/
 
-Pagy.options[:limit] = 25
-Pagy.options[:client_max_limit] = 100
+Pagy::OPTIONS[:limit] = 25
+Pagy::OPTIONS[:client_max_limit] = 100
 
 Rails.application.config.assets.paths << Pagy::ROOT.join("stylesheets")
